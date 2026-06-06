5d1673211d8efb9ab4aaa0ef6ce2f22983b81f2c
Fahimun_Is <fahimunislamlamia@gmail.com>
2026-06-06 21:00:11 +0600

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,15 @@
+// Task 2: Data Types Check
+
+let name = "Fahimun Islam";
+let age = 24;
+let isStudent = true;
+let district = "Gazipur";
+let salary = null;
+let phone = "01720172503";
+
+console.log("name     →", typeof name); // string
+console.log("age      →", typeof age); // number
+console.log("isStudent→", typeof isStudent); // boolean
+console.log("district →", typeof district); // string
+console.log("salary   →", typeof salary); // object (JavaScript quirk)
+console.log("phone    →", typeof phone); // string
---
+++ student.js
@@ -0,0 +1,15 @@
+// Task 1: Student Information
+
+let name = "Fahimun Islam";
+let age = 24;
+let isStudent = true;
+let district = "Gazipur";
+let salary = null;
+let phone = "01720172503";
+
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("Phone: " + phone);
+console.log("District: " + district);
+console.log("Student: " + isStudent);
+console.log("Salary: " + salary);
